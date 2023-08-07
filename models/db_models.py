5ca76ab844539eb775d8c30cf93e551567857c53
Danila Deryavko <deryavkodan@gmail.com>
2023-08-07 21:51:35 +0700

    Changed indexes for database models (db_models.py)

--- models/db_models.py
+++ models/db_models.py
@@ -43,7 +43,7 @@ class BaseEventTelemetryNetwork(BaseModel):
 
     class Meta:
         indexes = (
-            (('record_dtime', 'event'), True),
+            (('record_dtime', 'event'), False),
         )
 
     @staticmethod
